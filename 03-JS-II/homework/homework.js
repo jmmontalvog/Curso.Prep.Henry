669e511d0bc152bbdc5fcf93ee1e6f57bd6b25e4
unknown <jmmontalvog01@gmail.com>
2021-10-08 13:57:23 -0500

    I finished JSII.test.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -163,16 +163,18 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
+  if (numero < 2)
+    return false;
+  if(numero === 2)
+    return true;
+
   for (let i = 2; i < numero; i++) {
-    if (numero % i === 0 || numero % i === 1){
-      if (numero % 3 === 0)
-        return false;
-      else
-        return true;
+    if (numero % i === 0){
+      return false;
     }
-    else
-      return true;
   }
+  return true;
+
 }
 
 function esVerdadero(valor){
@@ -189,8 +191,14 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
+
+  let tableSix = [];
+
+  for(let i = 0; i < 11; i++){
+    tableSix.push(6 * i);
+  }
   
-  return tablaDelSeis = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]; 
+  return tableSix;
    
 }
 
